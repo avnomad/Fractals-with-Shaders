55aec666c82abb242262bfebe6a4234ac00d8dba
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2010-01-30 12:56:08 +0000

    integrated direct input. added the ability to resize, pan and zoom. removed garbage from fragment shader. changed fragment shader to compute mandelbrot set.
    --HG--
    extra : convert_revision : svn%3A147de9c3-2cbc-b044-a23a-24e03ba1b3f6/trunk%404

--- Shadered Fractals/main.cpp
+++ Shadered Fractals/main.cpp
@@ -1,6 +1,7 @@
 #include <gl/glew.h>
 #include <gl/glut.h>
 #include <cstdlib>
+#include <cmath>
 
 #include <iostream>
 using std::cout;
@@ -30,16 +31,110 @@ using std::string;
 using std::back_inserter;
 
 #include <char reader.h>
+#include <Direct Input/DirectInput Wrapper.h>
+#include <Direct Input/Mouse Wrapper.h>
+
+/* just grouping all the global variables in a global object g. */
+struct Global
+{
+	// fields
+	DirectInput directInput;
+	DIMouse mouse;
+
+	int window_width;
+	int window_height;
+	static const char *const window_title;
+
+	int mouse_z;
+	static const float wheel_factor;	// ln(10)/10
+	float fractal_center_x;
+	float fractal_center_y;
+
+	// uniforms
+	float s;
+	GLint s_index;
+	float b[2];
+	GLint b_index;
+
+	// methods
+	Global()
+		:directInput(GetModuleHandle(NULL)),
+		mouse(directInput)
+	{
+		window_width = 800;
+		window_height = 800;
+		fractal_center_x = 0;
+		fractal_center_y = 0;
+		mouse_z = 9592;	// ln(0.004)/(ln(10)/4000)
+
+		s = exp(wheel_factor*mouse_z);	// 0.004
+		b[0] = fractal_center_x - 0.5f*s*window_width;
+		b[1] = fractal_center_y - 0.5f*s*window_height;
+
+	} // end Global default constructor
+
+} g; // end struct Global
+
+const char *const Global::window_title = "drawing fractals using shaders";
+const float Global::wheel_factor = -0.000575646f;	// ln(10)/4000
+					// 120 is the smallest change in mouse_z
+
+
 
 void display()
 {
 	//glClear(GL_COLOR_BUFFER_BIT);
 
-	glColor3f(0,0,0.5);
+	glColor3f(1,0,0);
 	glRecti(-1,-1,1,1);
 
 	glutSwapBuffers();
 } // end function display
+
+
+void idle()
+{
+	bool change = false;
+
+	g.mouse.getState();
+	if(g.mouse.Z != 0)
+	{
+		g.mouse_z += g.mouse.Z;
+		g.s = exp(g.wheel_factor*g.mouse_z);
+		glUniform1f(g.s_index,g.s);
+
+		change = true;
+	} // end if
+
+	if(g.mouse.button[0] & 0x80)
+	{
+		if(g.mouse.X != 0)
+		{
+			g.fractal_center_x -= g.s*g.mouse.X;
+			change = true;
+		} // end if
+
+		if(g.mouse.Y != 0)
+		{
+			g.fractal_center_y += g.s*g.mouse.Y;
+			change = true;
+		} // end if
+	} // end if
+
+	if(change)
+	{
+		g.b[0] = g.fractal_center_x - 0.5f*g.s*g.window_width;
+		g.b[1] = g.fractal_center_y - 0.5f*g.s*g.window_height;
+		glUniform2fv(g.b_index,1,g.b);
+
+		system("CLS");
+		cout << "fractal center x:\t" << g.fractal_center_x << '\n';
+		cout << "fractal center y:\t" << g.fractal_center_y << '\n';
+		cout << "s zoom factor:\t\t" << g.s << '\n';
+
+		glutPostRedisplay();
+	} // end if
+} // end function active_motion
 
 
 void keyboard(unsigned char key, int x, int y)
@@ -52,10 +147,26 @@ void keyboard(unsigned char key, int x, int y)
 } // end function keyboard
 
 
-void reshape (int w, int h)
+void reshape(int w, int h)
 {
-	glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+	g.window_width = w;
+	g.window_height = h;
+
+	g.b[0] = g.fractal_center_x - 0.5f*g.s*g.window_width;
+	g.b[1] = g.fractal_center_y - 0.5f*g.s*g.window_height;
+	glUniform2fv(g.b_index,1,g.b);
+
+	glViewport(0,0,(GLsizei)w,(GLsizei)h);
 } // end function reshape
+
+
+void first_reshape(int w, int h)
+{
+	reshape(w,h);
+	g.mouse.setCooperativeLevel(FindWindow(0,g.window_title),DISCL_NONEXCLUSIVE | DISCL_BACKGROUND);
+	g.mouse.acquire();
+	glutReshapeFunc(reshape);
+} // end function first_reshape
 
 
 int main(int argc, char **argv)
@@ -63,9 +174,9 @@ int main(int argc, char **argv)
 	// glut initialization
 	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-	glutInitWindowSize(320,320);
+	glutInitWindowSize(g.window_width,g.window_height);
 	glutInitWindowPosition(800,100);
-	glutCreateWindow("drawing fractals using shaders");
+	glutCreateWindow(g.window_title);
 
 	// glew initialization
 	glewInit();
@@ -148,10 +259,17 @@ int main(int argc, char **argv)
 	glUseProgram(program);
 	cout << "done\n";
 
+	g.s_index = glGetUniformLocation(program,"s");
+	g.b_index = glGetUniformLocation(program,"b");
+
+	glUniform1f(g.s_index,g.s);
+	glUniform2fv(g.b_index,1,g.b);
+
 	// event handling initialization
 	glutDisplayFunc(display);
 	glutKeyboardFunc(keyboard);
-	glutReshapeFunc(reshape);
+	glutReshapeFunc(first_reshape);
+	glutIdleFunc(idle);
 	glutMainLoop();
 	return 0;
 } // end function main
--- Shadered Fractals/mandelbrot.frag
+++ Shadered Fractals/mandelbrot.frag
@@ -1,44 +1,25 @@
-// simple fragment shader
+uniform float s;
+uniform vec2 b;
 
-// 'time' contains seconds since the program was linked.
-uniform float time;
-
-#define width 1000
-#define height 800
 #define MAXITER 255
 #define THRESHOLD 25
-#define MAX_X 0.8
-#define MIN_X -2.2
-#define MAX_Y 1.2
-#define MIN_Y -1.2
+
 
 void main()
 {
-	float x = gl_FragCoord.x;
-	float y = gl_FragCoord.y;
-	mat2 m = mat2(0.005);
+	vec2 z = vec2(0,0);
+	vec2 c = s*gl_FragCoord.xy + b;
 	
-	//vec2 point = vec2(x*((MAX_X-MIN_X)/width)+MIN_X,y*((MAX_Y-MIN_Y)/height)+MIN_Y);
-	//vec2 z = vec2(0,0);
-	vec2 z = vec2(x*((MAX_X-MIN_X)/width)+MIN_X,y*((MAX_Y-MIN_Y)/height)+MIN_Y);
-	vec2 point = vec2(0.4,-0.15);
-
-	//point = m*point;
-	//point += vec2(-1.60,0.0);
-	
-	int c;
-	for(c = 0 ; c < MAXITER ; ++c)
+	int i;
+	for(i = 0 ; i < MAXITER ; ++i)
 	{
-		z = vec2(z.x*z.x-z.y*z.y,2.0*z.x*z.y)+point;
-		if (z.x*z.x+z.y*z.y>THRESHOLD)
+		z = vec2(z.x*z.x-z.y*z.y,2.0*z.x*z.y)+c;
+		if (dot(z,z)>THRESHOLD)
 			break;
 	}
-	float factor = float(c)/MAXITER;
+	float factor = float(i)/MAXITER;
 
 	gl_FragColor = vec4(factor,factor,0.5,1.0);
-
-	/*if(mod(floor(x/10),2)==0 ^^ mod(floor(y/10),2)==0)
-		gl_FragColor = vec4(0,0.5,1,1);
-	else
-		gl_FragColor = vec4(0,0,0,1);*/	
+	
+	if(length(c)<0.005) gl_FragColor = vec4(1,0,0,1);
 }
